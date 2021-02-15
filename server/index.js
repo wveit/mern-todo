a6cc92f0b06b5919461a52a3c5727186836e7bd8
wveit <wilbertveit@rocketmail.com>
2021-02-15 15:38:22 -0800

    Monorepo has package.json with npm scripts
    npm install installs deps for server and client
    npm build builds the client
    npm run dev runs server and client in dev mode
    npm start runs server in production mode

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 
 app.get('/api/greeting', (req, res) => {
-    setTimeout(() => res.send('hello'), 1000);
+    setTimeout(() => res.send('howdy'), 1000);
 });
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../client/build'));
+}
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, console.log(`Server started on ${PORT}.`));
